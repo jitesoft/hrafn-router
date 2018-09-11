043defaa2dce73f885497e29bf53f04fad0694ae
Johannes <johannes@jitesoft.com>
2018-09-11 09:32:46 +0200

    Created a route tree manager.

---
+++ src/RouteTree/RouteTreeManager.php
@@ -0,0 +1,93 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  RouteTreeManager.php - Part of the router project.
+
+  © - Jitesoft 2018
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+namespace Hrafn\Router\RouteTree;
+
+use Hrafn\Router\Router;
+use Jitesoft\Exceptions\Logic\InvalidArgumentException;
+use Jitesoft\Utilities\DataStructures\Maps\SimpleMap;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * RouteTreeManager
+ * @author Johannes Tegnér <johannes@jitesoft.com>
+ * @version 1.0.0
+ */
+class RouteTreeManager implements LoggerAwareInterface {
+
+    /** @var SimpleMap */
+    private $rootNodes;
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger) {
+        $this->logger    = $logger;
+        $this->rootNodes = new SimpleMap();
+    }
+
+    /**
+     * Create a new or return existing root node.
+     *
+     * @param string $part
+     * @return Node
+     */
+    public function createOrGetRootNode(string $part): Node {
+        $this->logger->debug('{tag} Fetching or creating root node.', ['tag' => Router::LOG_TAG]);
+        if ($this->rootNodes->has($part)) {
+            $this->logger->debug('{tag} Node existed, returning route node with part {part}.', [
+                'tag'  => Router::LOG_TAG,
+                'part' => $part
+            ]);
+        } else {
+            $this->logger->debug('{tag} Route node did not exist, creating new node with part {part}.', [
+                'tag'  => Router::LOG_TAG,
+                'part' => $part
+            ]);
+
+            $this->rootNodes[$part] = new Node(null, $part);
+        }
+
+        return $this->rootNodes[$part];
+    }
+
+    /**
+     * Create or get an existing node from a given node.
+     *
+     * @param Node   $parent
+     * @param string $part
+     * @return Node
+     */
+    public function createOrGetNode(Node $parent, string $part): Node {
+        if ($parent->hasChild($part)) {
+            $this->logger->debug('{tag} Node had child with part {part} returning node.', [
+                'tag'  => Router::LOG_TAG,
+                'part' => $part
+            ]);
+        } else {
+
+            $this->logger->debug('{tag} No child with part {part} existed. Creating new node..', [
+                'tag'  => Router::LOG_TAG,
+                'part' => $part
+            ]);
+            $parent->createChild($part);
+        }
+        return $parent->getChild($part);
+
+    }
+
+    /**
+     * Sets a logger instance on the object.
+     *
+     * @param LoggerInterface $logger
+     *
+     * @return void
+     */
+    public function setLogger(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
+}
---
+++ tests/RouteTree/RouteTreeManagerTest.php
@@ -0,0 +1,87 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  RouteTreeManagerTest.php - Part of the router project.
+
+  © - Jitesoft 2018
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+namespace Hrafn\Router\Tests\RouteTree;
+
+use Hrafn\Router\RouteTree\Node;
+use Hrafn\Router\RouteTree\RouteTreeManager;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\NullLogger;
+use ReflectionClass;
+
+/**
+ * RouteTreeManagerTest
+ * @author Johannes Tegnér <johannes@jitesoft.com>
+ * @version 1.0.0
+ */
+class RouteTreeManagerTest extends TestCase {
+
+    public function testCreateRootNode() {
+        $manager    = new RouteTreeManager(new NullLogger());
+        $reflection = new ReflectionClass($manager);
+        $property   = $reflection->getProperty('rootNodes');
+
+        $property->setAccessible(true);
+        $this->assertEmpty($property->getValue($manager));
+
+        $node = $manager->createOrGetRootNode('test');
+        $this->assertEquals('test', $node->getPart());
+
+        $this->assertCount(1, $property->getValue($manager));
+        $this->assertEquals($node, $property->getValue($manager)[$node->getPart()]);
+
+        $property->setAccessible(false);
+    }
+
+    public function testGetRootNode() {
+        $manager    = new RouteTreeManager(new NullLogger());
+        $reflection = new ReflectionClass($manager);
+        $property   = $reflection->getProperty('rootNodes');
+        $manager->createOrGetRootNode('root');
+
+        $property->setAccessible(true);
+        $node = $manager->createOrGetRootNode('root');
+
+        $this->assertEquals('root', $node->getPart());
+        $this->assertCount(1, $property->getValue($manager));
+        $node = $manager->createOrGetRootNode('root');
+        $this->assertEquals('root', $node->getPart());
+        $this->assertCount(1, $property->getValue($manager));
+        $property->setAccessible(false);
+    }
+
+    public function testCreateNode() {
+        $parent  = new Node(null, 'root');
+        $manager = new RouteTreeManager(new NullLogger());
+        $child   = $manager->createOrGetNode($parent, 'part');
+
+        $this->assertEquals($child, $parent->getChild('part'));
+        $this->assertEquals($parent, $child->getParent());
+        $this->assertEquals('part', $child->getPart());
+    }
+
+    public function testGetNode() {
+        $parent  = new Node(null, 'root');
+        $manager = new RouteTreeManager(new NullLogger());
+        $child   = $manager->createOrGetNode($parent, 'part');
+
+        $reflection = new ReflectionClass($parent);
+        $property   = $reflection->getProperty('children');
+        $property->setAccessible(true);
+        $this->assertCount(1, $property->getValue($parent));
+
+        $child2 = $manager->createOrGetNode($parent, 'part');
+
+        $this->assertCount(1, $property->getValue($parent));
+        $this->assertSame($child, $child2);
+        $this->assertEquals($child, $parent->getChild('part'));
+        $this->assertEquals($parent, $child->getParent());
+        $this->assertEquals('part', $child->getPart());
+
+        $property->setAccessible(false);
+    }
+
+}
