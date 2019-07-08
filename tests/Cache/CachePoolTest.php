ab6dd4b94f5880fb5c2e37cef406deee161cc287
Johannes Tegnér <johannes@jitesoft.com>
2019-07-08 19:54:04 +0200

    Added methods for the CacheItemTest cases.

--- tests/Cache/CachePoolTest.php
+++ tests/Cache/CachePoolTest.php
@@ -10,4 +10,42 @@ use PHPUnit\Framework\TestCase;
 
 class CachePoolTest extends TestCase {
 
+    public function testCreateWithoutFile() {}
+
+    public function testCreateWithFile() {}
+
+    public function testGetItem() {}
+
+    public function testGetItemInvalidKey() {}
+
+    public function testGetItemNotExist() {}
+
+    public function testGetItems() {}
+
+    public function testGetItemsInvalidKey() {}
+
+    public function testGetItemsNotFound() {}
+
+    public function testHasItem() {}
+
+    public function testHasItemInvalidKey() {}
+
+    public function testClear() {}
+
+    public function testDeleteItem() {}
+
+    public function testDeleteItemInvalidKey() {}
+
+    public function testDeleteItemNotFound() {}
+
+    public function testDeleteItems() {}
+
+    public function testDeleteItemsInvalidKey() {}
+
+    public function testSave() {}
+
+    public function testSaveDeferred() {}
+
+    public function testSaveAndCommit() {}
+
 }
