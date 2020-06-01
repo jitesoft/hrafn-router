6ec6885bd70fc1622d4333981baa4a14c23b3e49
Johannes Tegnér <johannes@jitesoft.com>
2020-06-01 21:00:45 +0200

    Fixed trait for middleware hadling.

--- src/RequestHandler/HandleMiddlewareTrait.php
+++ src/RequestHandler/HandleMiddlewareTrait.php
@@ -8,6 +8,7 @@ namespace Hrafn\Router\RequestHandler;
 
 use Hrafn\Router\Action;
 use Hrafn\Router\Router;
+use Jitesoft\Exceptions\Http\Server\HttpInternalServerErrorException;
 use Psr\Http\Message\RequestInterface;
 
 /**
@@ -23,6 +24,7 @@ trait HandleMiddlewareTrait {
      *
      * @param RequestInterface $request Request being handled.
      * @return mixed
+     * @throws HttpInternalServerErrorException If middleware was a string which could not be resolved.
      */
     public function process(RequestInterface $request) {
         if (count(Router::$disabledMiddleware) > 0) {
@@ -37,9 +39,18 @@ trait HandleMiddlewareTrait {
             }
         }
 
-        return $this->action
-            ->getMiddlewares()
-            ->dequeue()
+        $middleware = $this->action->getMiddlewares()->dequeue();
+        if (is_string($middleware)) {
+
+            if($this->container->has($middleware)) {
+                $middleware = $this->container->get($middleware);
+            } else {
+                throw new HttpInternalServerErrorException('Middleware {name} was not found in the container nor was it an instance.');
+            }
+
+        }
+
+        return $middleware
             ->process(
                 $request,
                 $this
